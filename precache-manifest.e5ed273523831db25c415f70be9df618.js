c5c331ba8c1749a9a383fb1beeba8269d721a384
Krystian Kordaszewski <kordaszewskikrystian@gmail.com>
2020-07-29 22:02:57 +0200

    Updates

---
+++ precache-manifest.e5ed273523831db25c415f70be9df618.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b52fa30f38a290cc82f15a851993373",
+    "url": "/kk-player-task/index.html"
+  },
+  {
+    "revision": "b3f42a28a077d60e4c8d",
+    "url": "/kk-player-task/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "b3f42a28a077d60e4c8d",
+    "url": "/kk-player-task/static/js/2.a3ce7aba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kk-player-task/static/js/2.a3ce7aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150a3a1ecdffc09dbc17",
+    "url": "/kk-player-task/static/js/main.21134b1b.chunk.js"
+  },
+  {
+    "revision": "333c8b14d4d0e9b3c5c0",
+    "url": "/kk-player-task/static/js/runtime-main.4ae6856d.js"
+  },
+  {
+    "revision": "d522478d9c40ce1d72360effa1c66521",
+    "url": "/kk-player-task/static/media/Play_active.d522478d.png"
+  },
+  {
+    "revision": "6d8b10090438264aa26fc7631331c427",
+    "url": "/kk-player-task/static/media/back_ico.6d8b1009.svg"
+  },
+  {
+    "revision": "52d51b7d5b834b033266df7aeaa497b9",
+    "url": "/kk-player-task/static/media/bg_image.52d51b7d.jpg"
+  },
+  {
+    "revision": "8e344a799b1e4ac1ec57619e7ae0e4f4",
+    "url": "/kk-player-task/static/media/cover-1.8e344a79.png"
+  },
+  {
+    "revision": "aa246fdc4720067bd23be8489b4e7c8a",
+    "url": "/kk-player-task/static/media/cover.aa246fdc.png"
+  },
+  {
+    "revision": "efd35a48d3758a96543e17f5e6d24850",
+    "url": "/kk-player-task/static/media/hide_ico.efd35a48.svg"
+  },
+  {
+    "revision": "68d542b29051b7d6c682709210fb9042",
+    "url": "/kk-player-task/static/media/more_ico.68d542b2.svg"
+  },
+  {
+    "revision": "908e77adf0762e3ca48d8da50ad268d9",
+    "url": "/kk-player-task/static/media/next_ico.908e77ad.svg"
+  },
+  {
+    "revision": "d560ea63609f1bd760297025444ffa79",
+    "url": "/kk-player-task/static/media/playlist_ico.d560ea63.svg"
+  },
+  {
+    "revision": "61530e1d25eb1ef8ad5d7e15af9ac18a",
+    "url": "/kk-player-task/static/media/previous_ico.61530e1d.svg"
+  },
+  {
+    "revision": "f24e65322df49b5a8fd3c450d993a60d",
+    "url": "/kk-player-task/static/media/repeat_ico.f24e6532.svg"
+  },
+  {
+    "revision": "2ca80224c6de3f27c3db8ebc8a7444b3",
+    "url": "/kk-player-task/static/media/sample.2ca80224.mp3"
+  },
+  {
+    "revision": "9cca0f11fe187d5900f978490e200dd4",
+    "url": "/kk-player-task/static/media/shuffle_ico.9cca0f11.svg"
+  },
+  {
+    "revision": "a21957684324528264df44116fc5c48c",
+    "url": "/kk-player-task/static/media/unreleased_cover.a2195768.png"
+  }
+]);
